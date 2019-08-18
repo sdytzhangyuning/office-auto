4eafb8dd38e63c5bd8604636a4c350859d6cdac1
yuning <zynsdyt@gmail.com>
2019-08-18 16:03:10 -0400

    update

---
+++ src/com/util/login.java
@@ -0,0 +1,21 @@
+package com.util;
+
+import com.entity.Userinfo;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class login extends AbstractInterceptor {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        // TODO Auto-generated method stub
+        Userinfo user = (Userinfo) actionInvocation.getInvocationContext().getSession().get("loginuser");
+        if (user != null) {
+            return actionInvocation.invoke();
+        }
+        return "loginError";
+    }
+
+}
